02ca57ef1585b6f66cf6873f9e5dce42fc52b2f4
Harsh <harsh.devavanshi@autozone.com>
2024-07-05 12:08:23 +0530

    Updates

---
+++ precache-manifest.41fc833c27e7e9f92f48b14e5cab85f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948bc480be31d0aa50e8aeb2ee36dd10",
+    "url": "/job-portal/index.html"
+  },
+  {
+    "revision": "08a13faa8d73dfaa6745",
+    "url": "/job-portal/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "43534ed3578f4a9c80aa",
+    "url": "/job-portal/static/js/2.70cf6e69.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/job-portal/static/js/2.70cf6e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f84646a7ba524633d5",
+    "url": "/job-portal/static/js/3.80fd407f.chunk.js"
+  },
+  {
+    "revision": "08a13faa8d73dfaa6745",
+    "url": "/job-portal/static/js/main.4982a001.chunk.js"
+  },
+  {
+    "revision": "104ab9b6514855b25e84",
+    "url": "/job-portal/static/js/runtime-main.e8afe22f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/job-portal/static/media/logo.06e73328.svg"
+  }
+]);
